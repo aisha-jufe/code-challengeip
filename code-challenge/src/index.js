f4a048af1a7c2b477539b57fdea8f639c893efec
Aisha <117970818+aisha-jufe@users.noreply.github.com>
2022-12-06 21:40:25 +0300

    Add files via upload

---
+++ code-challenge/src/index.js
@@ -0,0 +1,43 @@
+const character = document.querySelector("#character-bar");
+const container = document.getElementById("detailed-info")
+const animalName = document.getElementById("name");
+const image = document.getElementById("image");
+const form = document.getElementById("votes-form")
+const animalVotes = document.getElementById("vote-count");
+const input = document.getElementById("votes");
+const resetVotes = document.getElementById('reset-btn');
+resetVotes.style.cursor = 'pointer';
+let currentAnimal;
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    currentAnimal.votes += parseInt(e.target.votes.value);
+    showAnimal(currentAnimal);
+});
+resetVotes.addEventListener('click', () => {
+    currentAnimal.votes = 0;
+    showAnimal(currentAnimal);
+})
+
+function getCharacters() {
+    fetch('http://localhost:3000/characters/')
+    .then(response => response.json())
+    .then(renderAnimals)
+}
+getCharacters();
+function renderAnimals(animals) {
+    animals.forEach(renderCharacters)
+}function renderCharacters(animal) {
+    const spanEle = document.createElement('span')
+    spanEle.innerHTML = animal.name;
+    spanEle.style.cursor = 'pointer';
+    character.appendChild(spanEle)
+    spanEle.addEventListener('click', () => {
+        currentAnimal = animal;
+        showAnimal(animal);
+})
+}
+let showAnimal = function(animal){
+    animalName.innerText=animal.name;
+    image.src = animal.image;
+    animalVotes.innerText = animal.votes;
+}
